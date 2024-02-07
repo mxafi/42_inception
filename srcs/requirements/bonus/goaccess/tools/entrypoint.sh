424c60ce1e97d1d9136e8bee7df6366eccf4da24
Markus Laaksonen <markus@mxa.fi>
2024-02-07 19:00:40 +0200

    fix goaccess wait for access.log
    goaccess fails to start if access.log does not exist or if it is incorrectly formatted,
    made it check for access.log every minute,
    goaccess still fails if access.log is incorrectly formatted

--- srcs/requirements/bonus/goaccess/tools/entrypoint.sh
+++ srcs/requirements/bonus/goaccess/tools/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+FILE=/var/log/nginx/access.log
+while [ ! -f "$FILE" ]; do
+  echo "$FILE does not exist. Sleeping for a minute."
+  sleep 60
+done
+
+echo "$FILE found, starting goaccess!"
+
 lighttpd -f /etc/lighttpd/lighttpd.conf
 
 exec "$@"
